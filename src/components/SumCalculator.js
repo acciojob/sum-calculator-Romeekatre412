054bc9444bb8bd61492c5dffc5308e6e3da0cdc6
Romee Katre <101556730+Romeekatre412@users.noreply.github.com>
2026-02-25 18:54:31 +0530

    Update SumCalculator.js

--- src/components/SumCalculator.js
+++ src/components/SumCalculator.js
@@ -1,1 +1,12 @@
+import React from "react";
+import SumCalculator from "./SumCalculator";
 
+function App() {
+  return (
+    <div>
+      <SumCalculator />
+    </div>
+  );
+}
+
+export default App;
